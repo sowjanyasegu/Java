d5f5d2a37a3633e2a740dfefed1584c9a644df56
sowjanyasegu <sowjanya.segu@gmail.com>
2021-07-18 15:11:58 +0530

    First Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+    void add(int a, int b){
+        System.out.println(a+b);
+    }
+    public static void main(String[] args) {
+	    Main mainObj= new Main();
+        mainObj.add(2,3);
+    }
+}
